2d47fd01fe87c3c2939282cbaea9a414a0e65fa1
Veiasai <o206@qq.com>
2021-02-10 06:56:34 +0000

    element plus

---
+++ src/views/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+    <el-input v-model="userName" placeholder="用户名"></el-input>
+    <el-input v-model="password" placeholder="密码"></el-input>
+    <el-button @click="login" >登录</el-button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+
+@Options({
+  components: {
+    HelloWorld
+  }
+})
+export default class Login extends Vue {}
+</script>
