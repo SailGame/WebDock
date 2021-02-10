6a9ec978f5f6c4d802daa30b32677c97a1f156d2
Veiasai <o206@qq.com>
2021-02-10 07:16:02 +0000

    login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,8 +1,14 @@
 <template>
-  <div class="about">
-    <el-input v-model="userName" placeholder="用户名"></el-input>
-    <el-input v-model="password" placeholder="密码"></el-input>
-    <el-button @click="login" >登录</el-button>
+  <div class="login">
+    <el-row>
+      <el-col :span="8"/>
+      <el-col :span="8">
+        <el-input v-model="userName" placeholder="用户名"></el-input>
+        <el-input v-model="password" placeholder="密码"></el-input>
+        <el-button @click="login" >登录</el-button>
+      </el-col>
+      <el-col :span="8"/>
+    </el-row>
   </div>
 </template>
 
@@ -13,7 +19,25 @@ import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 @Options({
   components: {
     HelloWorld
+  },
+  data: function () {
+    return {
+      userName: '',
+      password: ''
+    }
+  },
+  methods: {
+    login () {
+      console.log('123')
+    }
   }
 })
 export default class Login extends Vue {}
 </script>
+
+<style lang="less">
+  .login .el-input {
+    margin-bottom: 20px;
+  }
+
+</style>
